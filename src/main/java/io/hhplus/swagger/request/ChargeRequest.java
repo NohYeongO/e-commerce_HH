6c5010b848921b6920a23d75e88571caa9adc8fb
노영오 <nohyo05@naver.com>
2024-10-13 14:59:24 +0900

    스웨거 세팅

---
+++ src/main/java/io/hhplus/swagger/request/ChargeRequest.java
@@ -0,0 +1,16 @@
+package io.hhplus.swagger.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class ChargeRequest {
+
+    private Long userId;
+    private BigDecimal point;
+
+}
